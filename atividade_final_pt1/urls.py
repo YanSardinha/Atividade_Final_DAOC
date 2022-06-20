ae12b9562a19236d99444d07d0fddef6dd908f7b
Yan <yansardinha1001@gmail.com>
2022-06-20 13:27:38 -0300

    Adicionando form e view para login

--- atividade_final_pt1/urls.py
+++ atividade_final_pt1/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('rede_social.urls')),
+    path('conta/', include('django.contrib.auth.urls')),
 ]
